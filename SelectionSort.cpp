270b89e354227b354fdad099c7cbafe5fd74269d
Daniel Andreas Wang <gamesman36@outlook.com>
2023-05-07 19:18:59 +0200

    Adding selection sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+int smallest(std::vector<int> arr, int k, int n)
+{
+    int pos = k, small = arr[k], i;
+    for (i = k + 1; i < n; i++)
+    {
+        if (arr[i] < small)
+        {
+            small = arr[i];
+            pos = i;
+        }
+    }
+
+    return pos;
+}
+
+void SelectionSort(std::vector<int> arr)
+{
+    int s = arr.size();
+
+    int k, pos, temp;
+    for (k = 0; k < s; k++)
+    {
+        pos = smallest(arr, k, s);
+        temp = arr[k];
+        arr[k] = arr[pos];
+        arr[pos] = temp;
+    }
+
+    for(int i=0; i<arr.size(); i++) {
+        std::cout << arr[i] << "\n";
+    }
+}
+
+int main()
+{
+    std::vector<int> nums = {3, 17, 5, 10, 1, 9, 11};
+    int s = nums.size();
+    SelectionSort(nums);
+}
